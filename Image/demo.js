f751fdbe0642876750fcd4fa4809e6ae0138fb8a
Akash Chaudhari <akash.ac31@gmail.com>
2021-07-21 21:58:45 +0530

    first commit

---
+++ Image/demo.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
